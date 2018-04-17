3dc61a1b99f6de0fa86dc4498a5061043b278953
Mikkel <mhso@itu.dk>
2018-04-17 11:59:05 +0200

    One more for the road

--- TDD/SamplingTest.fs
+++ TDD/SamplingTest.fs
@@ -15,5 +15,6 @@ let allTest =
         let gridValues = 
             Array.sortWith (fun (x1, y1) (x2, y2) -> 
                 if x1 > x2 && y1 > y2 then 1 else if x1 = x2 && y1 = y2 then 0 else -1) gridValues
-        Array.forall2 (fun g1 g2 -> g1 = g2) gridExpected gridValues
+        Assert.True ((Array.forall2 (fun g1 g2 -> g1 = g2) gridExpected gridValues), 
+            "JitteredPropertyIsMaintained")
     jittered_JitteredPropertyIsMaintained
