90aea2fbfac05e3f30128a314df3c33f9c72b12d
Mathias <maoj@itu.dk>
2018-04-23 14:36:18 +0200

    Parser can parse all vertices

--- src/TriangleMeshes/PLYParser.fs
+++ src/TriangleMeshes/PLYParser.fs
@@ -1,29 +1,62 @@
 ﻿module PLYParser
 
 open FParsec
+open System.IO
 
 type UserState = unit
 type Parser<'t> = Parser<'t,UserState>
+type Vertex(x,y,z) = 
+    member this.x = x
+    member this.y = y
+    member this.z = z
+
 
 let parse parser str = 
     match run parser str with
     | Success(result,_,_) ->  result
     | Failure (errorMsg,_,_) ->  failwith("WRONG FORMAT")
 
-//let test = parseFloat pfloat "2.25"
-//let example = test * 2.
+let parseBool parser str = 
+    match run parser str with
+    | Success(_,_,_) ->  true
+    | Failure (_,_,_) ->  false
 
-let parseString s = pstring s
-let parseMultipleFloatsOneLine : Parser<_> = parseString "[" >>. sepBy pfloat (parseString ";") .>> parseString "]"
+let WhiteSpace = pstring " "
 
-//let str s = pstring s
-//let betweenStrings pBegin pEnd parser = pBegin >>. parser .>> pEnd
+let parsePLY (filepath:string) = 
+    let sr = new StreamReader(filepath)
+    let mutable parsing = true
+    if (not sr.EndOfStream) then
+        let mutable nextLine = sr.ReadLine()
+        let result = parseBool (pstring "ply") (nextLine)
+        match result with
+        | true -> 
+            nextLine <- sr.ReadLine()
+            let isAscii = parseBool (pstring "format ascii 1.0") (nextLine)
+            let formatBoolean = many ((pstring "format binary_little_endian 1.0") <|> (pstring "format binary_big_endian 1.0"))
+            let isBoolean = parseBool (formatBoolean) (nextLine)
+            match isAscii,isBoolean with
+            | true,_ -> 
+                nextLine <- sr.ReadLine()
+                let isComment (s:string) = parseBool (pstring "comment" .>> (anyString (s.Length-7))) s
+                while (isComment nextLine) do 
+                    printfn "%A" nextLine
+                    nextLine <- sr.ReadLine()
+                let arraySizeParser = pstring "element vertex " >>. pint32
+                let arraySize = parse arraySizeParser nextLine
+                let triangleArray : float list array = Array.zeroCreate arraySize
+                let isEndOfHeader s = parseBool (pstring "end_header") s
+                while (not (isEndOfHeader nextLine)) do
+                    printfn "NOT IMPLEMENTED"
+                    nextLine <- sr.ReadLine()
+                for i in 0..triangleArray.Length-1 do
+                    nextLine <- sr.ReadLine()
+                    nextLine <- nextLine.Substring(0,nextLine.Length-1)
+                    let listFloatParser = (sepBy pfloat WhiteSpace)
+                    let listFloat = parse listFloatParser nextLine
+                    triangleArray.[i] <- listFloat
+                printfn "Array Of Vertices done"
+            | _,true -> printfn ("BINARY START")
+            | _,_ -> printfn ("WRONG FORMAT")
 
-let stringToFloatList : Parser<_> = (sepBy pfloat (parseString " "))
-let stringToFloatListList : Parser<_> = sepBy (sepBy pfloat (parseString " ")) (parseString ("\n"))
-
-//let hardCodeFloatList : Parser<_> = pfloat .>> pstring " " >>. pfloat .>> pstring " " >>. pfloat .>> pstring " " >>. pfloat .>> pstring " " >>. pfloat .>> " "
-
-
-//let parseString = sepBy pstring (parseString " ")
-
+        | false -> parsing <- false
--- src/TriangleMeshes/Program.fs
+++ src/TriangleMeshes/Program.fs
@@ -5,7 +5,7 @@ open FParsec
 
 [<EntryPoint>]
 let main argv = 
-    let result = PLYParser.parse PLYParser.stringToFloatList "-0.0378297 0.12794 0.00447467 0.850855 0.5 "
-    printfn "%A" result
+    let answer = PLYParser.parsePLY @"D:\Users\Mathias\Documents\GIT-ITU\SecondYearProject\raytracer\resources\ply\bunny.ply"
+    //printfn "%A" answer
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
