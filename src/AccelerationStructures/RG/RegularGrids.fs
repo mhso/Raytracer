eae55acec4c35b146de7bc4ef18d85ac1784954e
Frank Andersen <fand@itu.dk>
2018-04-29 20:58:41 +0200

    Before pull from dev

--- src/AccelerationStructures/RG/RegularGrids.fs
+++ src/AccelerationStructures/RG/RegularGrids.fs
@@ -5,3 +5,16 @@ let clamp (x:float,b:float) =
     | x when x<0. -> 0.
     | x when x>b -> b
     | _ -> x
+
+let calcEdgeLength (wx:float) (wy:float) (wz:float) (n:float) : float = System.Math.Pow (((wx*wy*wz)/n),(1./3.))
+
+let calcAxisCell (m:float) (w:float) (s:float) : float = ((m*w)/s)+1.
+
+let calcAxisCells (wx:float) (wy:float) (wz:float) (m:float) (n:float) = 
+    let s =  calcEdgeLength wx wy wz n
+    let nx = calcAxisCell m wx s
+    let ny = calcAxisCell m wy s
+    let nz = calcAxisCell m wz s
+    (nx, ny, nz)
+
+    
