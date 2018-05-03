d6495356e8629ddd3eaee9a5deb6a6ad8350ca8a
Mikkel <mhso@itu.dk>
2018-05-03 14:22:28 +0200

    Fixed async color error

--- src/Basics/Scene.fs
+++ src/Basics/Scene.fs
@@ -37,9 +37,8 @@ type Scene(shapes: Shape list, camera: Camera, lights: Light list) =
                                                        ░                                        
                                                                                                 ")
         
-        
         let timer = System.Diagnostics.Stopwatch.StartNew()
-        
+
         let pos = [for y in 0 .. camera.ResY - 1 do
                     for x in 0 .. camera.ResX - 1 do yield (x,y)]
 
@@ -48,31 +47,35 @@ type Scene(shapes: Shape list, camera: Camera, lights: Light list) =
         Parallel.ForEach (pos, fun (x,y) -> 
             let rays = camera.CreateRays x y
             let cols = List.map (fun ray -> (camera.Cast ray backgroundColour shapes lights)) rays
-            let colour = (List.fold (+) Colour.Black cols)/float colours.Length
+            let colour = (List.fold (+) Colour.Black cols)/float cols.Length
             colours.[y,x] <- colour) |> ignore
 
         for y in 0 .. camera.ResY - 1 do
           for x in 0 .. camera.ResX - 1 do
+            if colours.[y,x].R > 0.0 && colours.[y,x].G > 0.0 && colours.[y,x].B > 0.0 then printfn "Wow"
             renderedImage.SetPixel(x, y, colours.[y,x].ToColor)
 
         (*
         for x in 0..camera.ResX-1 do
             for y in 0..camera.ResY-1 do
                 let pct = int((float (x*y)/total) * 100.0)
+
                 // Progress bar!!!
                 if pct > currPct then 
                     currPct <- pct
+
                     let dots = String.replicate (currPct/2 + 1) "█"
                     let white = String.replicate (49-(currPct/2)) "░"
                     loadingIndex <- loadingIndex + 1
                     if loadingIndex = loadingSymbols.Length then loadingIndex <- 0
+
                     Console.Write("\r                                {0}", loadingSymbols.[loadingIndex] + " |" + dots + white + "| " + string (pct+1) + "%");
                     
-                let ray = camera.CreateRay x y
-                let colour = camera.Cast ray backgroundColour shapes lights
-                renderedImage.SetPixel(x, y, colour.ToColor)
+                let rays = camera.CreateRays x y
+                let colours = List.map (fun ray -> (camera.Cast ray backgroundColour shapes lights)) rays
+                let colour = (List.fold (+) Colour.Black colours)/float colours.Length
+                renderedImage.SetPixel(x, y, colour.ToColor)*)
                 
-*)
         // Save image
         renderedImage.Save(camera.RenderFilepath)
         
