3c05c20b8cdc805659ba6fd2c48bd42334935103
Nicolai <ndje@itu.dk>
2018-05-07 11:42:41 +0200

    I think interfacing for acceleration works now!

---
+++ src/Basics/Acceleration.fs
@@ -0,0 +1,14 @@
+﻿namespace Tracer.Basics
+
+module Acceleration = 
+    open KD_tree
+
+    type Acceleration = KDTree of KDTree
+                      | BVH of float // Swap with actual BVH
+                      | RG of float // Swap with actual RG
+
+    let setAcceleration (accel:Acceleration) (ray:Ray) (shapes:array<Shape>) = 
+        match accel with
+        | KDTree(kdTree) -> traverseKDTree kdTree ray shapes
+        | BVH(bvh) -> failwith "Not Implemented"
+        | RG(rg) -> failwith "Not Implemented"
