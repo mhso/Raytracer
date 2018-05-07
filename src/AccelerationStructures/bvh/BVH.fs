e2313120f0286d0545765032b97805c5be6310bb
Frank Andersen <fand@itu.dk>
2018-05-07 10:08:32 +0200

    refactor and documentation.

--- src/AccelerationStructures/bvh/BVH.fs
+++ src/AccelerationStructures/bvh/BVH.fs
@@ -3,36 +3,38 @@
 module BVH = 
     open Tracer.Basics
     
+    // Used for debug, will print to console etc. 
     let debug = true
 
-    (* BVH TREE *)
-    type BVHtree = 
-        | Leaf of List<int> * BBox
-        | Node of BVHtree * BVHtree * BBox * int
-
+     // Type of the BVHTree, with Nodes and Leafs.
+    type BVHtree = | Leaf of List<int>*BBox
+                   | Node of BVHtree*BVHtree*BBox*int
+    
+    // Function for sorting a int list, based on array of bounding boxes and axis value.
     let rec sortListByAxis (indexList:list<int>) (boxes:array<BBox>) (axis:int) =
       match indexList with
       | [] -> []
       | x :: xs ->
           let less, great = 
               match axis with
-              | 0 ->
-                    let filterLess = fun e -> boxes.[e].lowPoint.X <= boxes.[x].lowPoint.X
-                    let filterGreat = fun e -> boxes.[e].lowPoint.X >  boxes.[x].lowPoint.X
-                    filterLess, filterGreat
-              | 1 -> 
-                    let filterLess = fun e -> boxes.[e].lowPoint.Y <= boxes.[x].lowPoint.Y
-                    let filterGreat = fun e -> boxes.[e].lowPoint.Y >  boxes.[x].lowPoint.Y
-                    filterLess, filterGreat
-              | _ ->
-                    let filterLess = fun e -> boxes.[e].lowPoint.Z <= boxes.[x].lowPoint.Z
-                    let filterGreat = fun e -> boxes.[e].lowPoint.Z >  boxes.[x].lowPoint.Z
-                    filterLess, filterGreat
+              // Sort by x axis
+              | 0 -> let filterLess = fun e -> boxes.[e].lowPoint.X <= boxes.[x].lowPoint.X
+                     let filterGreat = fun e -> boxes.[e].lowPoint.X >  boxes.[x].lowPoint.X
+                     filterLess, filterGreat
+              // Sort by y axis
+              | 1 -> let filterLess = fun e -> boxes.[e].lowPoint.Y <= boxes.[x].lowPoint.Y
+                     let filterGreat = fun e -> boxes.[e].lowPoint.Y >  boxes.[x].lowPoint.Y
+                     filterLess, filterGreat
+              // Sort by z axis
+              | _ -> let filterLess = fun e -> boxes.[e].lowPoint.Z <= boxes.[x].lowPoint.Z
+                     let filterGreat = fun e -> boxes.[e].lowPoint.Z >  boxes.[x].lowPoint.Z
+                     filterLess, filterGreat
 
-          let lesser = sortListByAxis (xs |> List.filter(less)) (boxes) axis
-          let greater  = sortListByAxis (xs |> List.filter(great)) (boxes) axis
+          let lesser    = sortListByAxis (xs |> List.filter(less)) (boxes) axis
+          let greater   = sortListByAxis (xs |> List.filter(great)) (boxes) axis
           lesser @ [x] @ greater
     
+    // Function for getting combined outer low and high from a array og bounding boxes.
     let findOuterBoundingBoxLowHighPoints (boxes:array<BBox>) = 
         let lowX = Array.fold (fun acc (box:BBox) -> if box.lowPoint.X < acc then box.lowPoint.X else acc) infinity boxes
         let lowY = Array.fold (fun acc (box:BBox) -> if box.lowPoint.Y < acc then box.lowPoint.Y else acc) infinity boxes
@@ -42,24 +44,22 @@ module BVH =
         let highZ = Array.fold (fun acc (box:BBox) -> if box.highPoint.Z < acc then box.highPoint.Z else acc) infinity boxes
         
         Point(lowX, lowY, lowZ), Point(highX, highY, highZ)
-
+    
+    // Function for finding the longest axis lenght of a cube
     let findLargestBoundingBoxSideLengths (box:(Point*Point)) =
         let lowPoint, highPoint = box
-        
         let x = highPoint.X - lowPoint.X
         let y = highPoint.Y - lowPoint.Y
         let z = highPoint.Z - lowPoint.Z
         let mutable t = 0.
         let mutable value = (0, 0.)
 
-        if x > t then
-            value <- (0, x)
-        if y > t then
-            value <- (1, y)
-        if y < z then
-            value <- (2, z)
+        if x > t then value <- (0, x)
+        if y > t then value <- (1, y)
+        if y < z then value <- (2, z)
         value
-
+    
+    // Function to find the min-values from a bounding box based on the axis value.
     let findAxisMinMaxValues (bBox:BBox) axis =
         let lowPoint, highPoint = bBox.lowPoint, bBox.highPoint;
         match axis with
@@ -71,6 +71,7 @@ module BVH =
     let rec getBoxArrFromIndexes (indexes:list<int>) (boxes:array<BBox>) : (array<BBox>) =
         [|for i in 0..(indexes.Length-1) -> boxes.[i]|]
     
+    // Function for converting a list of shapes to an array of their bounding boxes.
     let convertShapesToBBoxes (shapes:array<Shape>) : array<BBox> =
         let bboxArr : BBox[] = Array.zeroCreate (shapes.Length)
         for i in 0..bboxArr.Length-1 do
@@ -98,13 +99,6 @@ module BVH =
                 let middle = sortedList.Length/2
                 let leftList = sortedList.[0..middle-1]
                 let rigthList = sortedList.[middle..]
-                
-                //printfn "innerNodeTree rec run... axisToSplit: %i, countRuns: %i" axisToSplit (treeLevel)
-                //printfn "Add new inner Nodes... Lists lenght: "
-                //printfn "intIndexes.Length: %i " intIndexes.Length
-                //printfn "boxArr.Length: %i " boxArr.Length
-                //printfn "leftList.Length: %i " leftList.Length
-                //printfn "rigthList.Length: %i " rigthList.Length
 
                 Node (
                             innerNodeTree leftList treeLevel, 
@@ -120,18 +114,19 @@ module BVH =
  
   // ######################### TRAVERSAL BVH TREE #########################
 
-    // swaps the order if d is not positive
+    // Function swaps the order if d is not positive
     let order d (left:BVHtree) (right:BVHtree) = 
         if debug then printfn "Call to order..."
         match d with
         | d when d > 0 -> (left, right)
         | _            -> (right, left)
 
-    // Checking for tree leaf.
+    // Function checking for tree leaf.
     let isLeaf = function
     | Leaf (_,_) -> true
     | _ -> false
 
+    // Function get ray direction based on the axis.
     let getRayDirectionValue (ray:Ray) (axis:int) =
         if debug then printfn "Call to getRayDirectionValue..."
         match axis with
@@ -148,24 +143,23 @@ module BVH =
         | Leaf (_,bbox) ->      if debug then printfn "getBox -> Leaf box \n %A" bbox
                                 bbox
 
+    // Functions finds closest hit of a ray in a tree.
     let closestHit (treeNode:BVHtree) (ray:Ray) (shapes:array<Shape>)  =
         if debug then printfn "Call to closestHit..."
         match treeNode with
-        | Leaf (shapesRef, _) -> 
-                            let mutable closestHit = None
-                            let mutable closestDist = infinity
-                            for shapeRef in shapesRef do
-                                let hit = shapes.[shapeRef].hitFunction ray
-                                let dist = hit.Time
-                                if dist < closestDist then
-                                    closestDist <- dist
-                                    closestHit <- Some hit
-                            if debug then printfn "closestHit -> Leaf found return hit at dist %f" closestDist
-                            closestHit
-        | _ -> 
-                if debug then printfn "closestHit -> None..."
+        |   Leaf (shapesRef, _) ->  let mutable closestHit = None
+                                    let mutable closestDist = infinity
+                                    for shapeRef in shapesRef do
+                                        let hit = shapes.[shapeRef].hitFunction ray
+                                        let dist = hit.Time
+                                        if dist < closestDist then
+                                            closestDist <- dist
+                                            closestHit <- Some hit
+                                    if debug then printfn "closestHit -> Leaf found return hit at dist %f" closestDist
+                                    closestHit
+        | _ ->  if debug then printfn "closestHit -> None..."
                 None
-
+    // Function performs recursive searh in the tree, with a maximum distance from the ray origin.
     let rec search (treeNode:BVHtree) (ray:Ray) (shapes:array<Shape>) (tmax:float) =
         if debug then printfn "Call to search with tmax: %f, lenght of array %i" tmax shapes.Length 
         if debug then printfn "Value of ray: %A" ray
@@ -177,9 +171,7 @@ module BVH =
         if value.IsNone then printfn "search -> Intersect is None..."
         if debug then printfn "Value of intersect: \n %A" value
         match value with  
-        | Some (t, t')  -> 
-                            printfn "match value -> Some (t, t') : t = %f  t' = %f" t t'
-                            
+        | Some (t, t')  ->  printfn "match value -> Some (t, t') : t = %f  t' = %f" t t'       
                             if (t<tmax) then 
                                 if isLeaf treeNode then
                                     if debug then printfn "(t<tmax) and isLeaf..."
@@ -206,7 +198,8 @@ module BVH =
                             else None
         | None -> if debug then printfn "search -> match value -> None"
                   None
-
+    
+    // Function for traversal of the tree.
     let traverse (treeNode:BVHtree) (ray:Ray) (shapes:array<Shape>) (tmax:float) = 
         if debug then printfn "Call to traverse..."
         search treeNode ray shapes infinity
