7dd8ae764092b7055ca2778190624b03b6884a8a
Mathias Oliver Valdbjørn Jørgensen <maoj@itu.dk>
2018-05-08 14:17:42 +0200

    Changed so that it parse the porsche. takes 200 seconds but works perfectly!

--- src/TriangleMeshes/Program.fs
+++ src/TriangleMeshes/Program.fs
@@ -30,8 +30,8 @@ let main argv =
     let emissive = EmissiveMaterial(Colour.White, 1.)
 
     //- SHAPES
-    let ico = (TriangleMes.drawTriangles  @"..\..\..\..\resources\ply\bunny10k.ply" false true matteWhite)
-    //let shape = Transform.transform ico (Transformation.scale 3. 3. 3.)
+    let shape = (TriangleMes.drawTriangles  @"..\..\..\..\resources\ply\porsche.ply" false true matteWhite)
+    //let shape = Transform.transform ico (Transformation.scale 50. 50. 50.)
 
     //- THIN LENS SAMPLE SETTINGS
     let CAM_SETS = 29
@@ -49,14 +49,14 @@ let main argv =
     let l3 = PointLight(Colour.White, 0.7, Point(-3.5, 12.0, 4.0))
 
     //- FINAL
-    let lights: Light list      = [lightAmbient; l1;l2;l3]
+    let lights: Light list      = [lightAmbient; l1;l2;l3; lightTop]
     let p1 = Point(1.,0.,0.)
     let p2 = Point(0.,4.,0.)
     let p3 = Point(0.,0.,2.)
 
 
 
-    let spheres: Shape array     = [|ico|]
+    let spheres: Shape array     = [|shape|]
     let scene                   = Scene(spheres, camera, lights, lightAmbient, 2)
 
 
