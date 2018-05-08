257bfa7c486481694f3c2f25a81ad1de3da2dfb9
ndje <ndje@itu.dk>
2018-05-08 10:42:52 +0200

    Rewrote Acceleration Interface. It should be fine now.

--- src/Basics/Acceleration.fs
+++ src/Basics/Acceleration.fs
@@ -3,12 +3,32 @@
 module Acceleration = 
     open KD_tree
 
-    type Acceleration = KDTree of KDTree
-                      | BVH of float // Swap with actual BVH
-                      | RG of float // Swap with actual RG
+    let mutable acceleration = ""
 
-    let setAcceleration (accel:Acceleration) (ray:Ray) (shapes:array<Shape>) = 
+    type IAcceleration = KDTree of KDTree
+                       | BVH of float // Swap with actual BVH
+                       | RG of float // Swap with actual RG
+
+    let createAcceleration acceleration (shapes:array<Shape>) = 
+        match acceleration with
+        | "KDTree" -> KDTree(buildKDTree shapes)
+        | "BVH"    -> failwith "Not Implemented"
+        | "RG"     -> failwith "Not Implemented"
+        | _        -> failwith "Unknown Acceleration Structure..."
+
+    let traverseIAcceleration (accel:IAcceleration) (ray:Ray) (shapes:array<Shape>) = 
         match accel with
         | KDTree(kdTree) -> traverseKDTree kdTree ray shapes
-        | BVH(bvh) -> failwith "Not Implemented"
-        | RG(rg) -> failwith "Not Implemented"
+        | BVH(bvh) -> failwith "Not Implemented" //Look above...
+        | RG(rg) -> failwith "Not Implemented" //Look above...
+
+
+    type Acceleration = KDTree
+                      | BVH
+                      | RG
+
+    let setAcceleration (accel : Acceleration) : unit = 
+        match accel with
+        | KDTree -> acceleration <- "KDTree"
+        | BVH -> acceleration <- "BVH"
+        | RG -> acceleration <- "RG"
