849bea5743dfc239d48f6e364adee42aba959da4
Nicolai <ndje@itu.dk>
2018-05-10 22:55:41 +0200

    Made the TestSuite work! Wohoo! Lots of... very slow... tests...

--- src/TracerTest/Program.fs
+++ src/TracerTest/Program.fs
@@ -16,15 +16,15 @@ let allTargets : Target list =
      AffineTransformations.render true;
      AffineTransformations.render false;
      ImplicitSurfaces.render;
-     Meshes.render KDTree;
+     Meshes.render Tracer.API.Acceleration.KDTree;
      Texture.render;
      Light.render;
      CSG.render;
      AmbientOcclusion.render [1;2;4;8;16]
      // The test groups below is only needed for teams of 7 students.
      // Teams of 6 students can uncomment the lines below.
-     Meshes.render RegularGrid;
-     Meshes.render BVH;
+     Meshes.render Tracer.API.Acceleration.RegularGrid;
+     Meshes.render Tracer.API.Acceleration.BVH;
      ]
 
 
