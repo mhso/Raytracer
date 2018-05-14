5761774210dd38553751ed390267a76d87317e53
Frank Andersen <fand@itu.dk>
2018-05-14 10:10:36 +0200

    merge conflicts solved?

--- src/Basics/Acceleration/Acceleration.fs
+++ src/Basics/Acceleration/Acceleration.fs
@@ -11,12 +11,29 @@ module Acceleration =
                        | BVHStructure of BVHStructure
                        | RGStructure of RGStructure
 
-    let createAcceleration (shapes:array<Shape>) = 
-        match acceleration with
-        | "KDTree" -> KDTree(buildKDTree shapes)
-        | "BVH"    -> BVHStructure (build shapes)
-        | "RG"     -> failwith "Not Implemented"
-        | _        -> KDTree(buildKDTree shapes) //Default...
+    type shapeArray (number: int, shapes:Shape array, acceleration:IAcceleration Option)  = 
+        member this.number = number
+        member this.shapes = shapes
+        member this.acceleration = acceleration
+
+    let mutable listOfKDTree : shapeArray list = []
+
+    let createAcceleration (shape: shapeArray) = 
+        if (listOfKDTree.Length < shape.number) then
+            let shapes = shape.shapes
+            match acceleration with
+            | "KDTree" -> 
+                let accel = KDTree(buildKDTree shapes)
+                listOfKDTree <- (shapeArray(shape.number,shape.shapes,Some accel))::listOfKDTree
+                printfn "Number of kdtrees %A" shape.number
+                accel
+            | "BVH"    ->
+                let accel = BVHStructure (BVH.build shapes)
+                listOfKDTree <- (shapeArray(shape.number,shape.shapes,Some accel))::listOfKDTree
+                accel
+            | "RG"     -> RGStructure (RegularGrids.build shapes)
+            | _        -> failwith "NOT A ACCELERATION TYPE"
+        else listOfKDTree.[shape.number-1].acceleration.Value
 
     let getAccelBoundingBox (accel:IAcceleration) = 
         match accel with
